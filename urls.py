c8db04e203d8b950b9c07ad1c8406d1cbdf1b60a
migbro <miguel.a.brown@gmail.com>
2015-12-07 17:05:24 -0600

    Still a mess but progress

--- urls.py
+++ urls.py
@@ -1,5 +1,8 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+import views
+from django.conf import settings
+from django.conf.urls import url, patterns
+from django.conf.urls.static import static
 
-urlpatterns = [
-]
+urlpatterns = patterns(('',
+                        url('r^viewer/mw_viewer', views.mw_viewer, name='home page'))
+                       ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- views.py
+++ views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+import json
+from datetime import datetime
 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.core.context_processors import csrf
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from models import *
 # Create your views here.
+def mw_viewer(request):
+    return render(request,'mw_viewer/viewer/mw_viewer_home.html')
